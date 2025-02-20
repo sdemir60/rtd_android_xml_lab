12e6e418d8b88a8b7e5ae07bb4431e90ca372f21
sdemir60 <sdemir@outlook.com.tr>
2025-02-20 17:12:29 +0300

    Project created, counter added.

---
+++ app/src/main/java/com/example/rtd_android_xml_lab/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.rtd_android_xml_lab
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private var counter = 0;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        initCounter();
+
+    }
+
+    private fun initCounter(){
+
+        val counterTextView = findViewById<TextView>(R.id.counterTextView)
+        val incrementButton = findViewById<Button>(R.id.incrementButton)
+
+        incrementButton.setOnClickListener{
+            counter++
+            counterTextView.text = counter.toString()
+        }
+
+    }
+}
